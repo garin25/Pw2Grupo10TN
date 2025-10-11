98a29bb4615b914bd170eb827d6f1b8599065b49
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-10-11 18:46:17 -0300

    Cambios en vista lobby

--- view/lobby.php
+++ view/lobby.php
@@ -8,6 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -78,20 +81,20 @@
 
 
             <!-- Main lobby -->
-            <section class="col-md-9 p-4" style="background-color: #3e045f;">
+            <section class="col-md-9 p-0 d-flex justify-content-center align-items-center" style="background-image: url(../img/texture-lobby.png); background-size: cover;">
 
-                <article class="row col-12 d-flex justify-content-center">
+                <article class="col-12 d-flex justify-content-center mt-3 align-items-center m-5 w-25 h-25 rounded-4" style="background-color: #FDF6E3; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.15), 
+              0px 6px 20px rgba(0, 0, 0, 0.2);">
+                    <div class="d-flex flex-column gap-4 justify-content-center">
+                            <h2 style="font-family: 'Nunito Sans'; font-weight: 900; color: #3d3024;text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.35);">PREGUNTADOS</h2>
+                            <button class="btn rounded-1 text-white" style="width: 250px; font-family: 'Nunito Sans'; font-weight: 900; background-color: #2A5A8C; border-color: #1A3858;">
+                                PARTIDA SOLO
+                            </button>
+                            <button class="btn rounded-1 text-white" style="width: 250px; font-family: 'Nunito Sans'; font-weight: 900; background-color: #8E7DBE; border-color: #6B5C9C;">
+                                PARTIDA MULTIJUGADOR
+                            </button>
+                        
 
-                </article>
-
-                <article class="row col-12 d-flex justify-content-center">
-                    <div class="d-flex gap-5 justify-content-center">
-                        <button class="btn btn-info text-white fw-bold">
-                            Partida solo
-                        </button>
-                        <button class="btn btn-info text-white fw-bold ">
-                            Partida multijugador
-                        </button>
                     </div>
                 </article>
 
