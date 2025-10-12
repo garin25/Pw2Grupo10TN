0c3e6c78b4ef62b073ffcdacecd72aee903d6b8d
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-10-12 19:38:36 -0300

    agregado login

---
+++ view/juego.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preguntas</title>
+    <link rel="stylesheet" href="../css/juego.css">
+</head>
+<body>
+    <main>
+        <section id="contenedor-preguntas">
+            <div id="preguntas-sup">
+                <img id="preguntas-img" src="..//img/logo.jpg" alt="">
+                <h2 id="preguntas-descripcion">Lorem, ipsum dolor sit amet consectetur adipisicing elit?</h2>
+            </div>
+            <div id="preguntas-inf">
+                <button id="a" class="btn">Opción</button>
+                <button id="b" class="btn">Opción</button>
+                <button id="c" class="btn">Opción</button>
+                <button id="d" class="btn">Opción</button>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
