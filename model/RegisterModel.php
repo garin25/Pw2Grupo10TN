cbab2c69eb31704ff7265be4bf966cd8221b2c84
franco <franco@gmail.com>
2025-10-17 19:51:42 -0300

    Se agrega vista,controlador y modelo de REGISTRO. Se muestra un error de mustache. Cuando se registra no se redirecciona correctamente al login V2

---
+++ model/RegisterModel.php
@@ -0,0 +1,20 @@
+<?php
+
+class RegisterModel
+{
+
+    private $conexion;
+
+    public function __construct($conexion)
+    {
+        $this->conexion = $conexion;
+    }
+
+    public function crearUsuario($nombreCompleto, $email, $passwordHash){
+        $sql = "INSERT INTO usuario (nombreCompleto, user, email, password) VALUES (?, ?, ?, ?)";
+
+        $this->conexion->registrarUsuario($sql, $nombreCompleto, $nombreCompleto, $email, $passwordHash);
+    }
+
+
+}
