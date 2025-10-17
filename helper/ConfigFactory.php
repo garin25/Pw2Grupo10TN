c9e00b695c7e982e4a3ecc923fe0248f90d65454
Leandro Jonatan Andriani <126698066+Leandro-hub24@users.noreply.github.com>
2025-10-16 23:02:16 -0300

    Update ConfigFactory.php

--- helper/ConfigFactory.php
+++ helper/ConfigFactory.php
@@ -6,10 +6,12 @@ include_once("controller/LoginController.php");
 include_once("controller/RankingController.php");
 include_once("controller/LobbyController.php");
 include_once("controller/JuegoController.php");
+include_once("controller/MiPerfilController.php");
 include_once("model/LoginModel.php");
 include_once("model/RankingModel.php");
 include_once("model/LobbyModel.php");
 include_once("model/JuegoModel.php");
+include_once("model/MiPerfilModel.php");
 include_once('vendor/mustache/src/Mustache/Autoloader.php');
 include_once ("helper/MustacheRenderer.php");
 
@@ -43,6 +45,8 @@ class ConfigFactory
         $this->objetos["LobbyController"] = new LobbyController(new LobbyModel($this->conexion), $this->renderer);
 
         $this->objetos["JuegoController"] = new JuegoController(new JuegoModel($this->conexion), $this->renderer);
+
+        $this->objetos["JuegoController"] = new MiPerfilController(new MiPerfilModel($this->conexion), $this->renderer);
 
 
     }
